45d230c59325d05850e8823ce59a285a90e332b2
lananh <anh626801@gmail.com>
2023-08-02 14:38:10 +0700

    jwt.sign

--- Backend/app/controller/login.controller.js
+++ Backend/app/controller/login.controller.js
@@ -1,6 +1,6 @@
 const { Account } = require("../models/index.model.js");
 const jwt = require("jsonwebtoken");
-
+const secret = "asdfghjkl!@#";
 exports.login = async (req, res, next) => {
   const { userName, password } = req.body;
   console.log(userName, password);
@@ -11,8 +11,19 @@ exports.login = async (req, res, next) => {
         password: password,
       },
     });
-    res.send({ message: documents, status: "success" });
+
+    if (documents) {
+      jwt.sign(documents["_id"], secret, function (err, data) {
+        return res.send({
+          message: "success",
+          token: data,
+          role: documents["role"],
+        });
+      });
+    } else {
+      res.send({ message: "fail", status: "fail" });
+    }
   } catch (error) {
-    res.send({ message: error, status: "faild" });
+    res.send({ message: error, status: "fail" });
   }
 };
